fc293f4183f85de16e5cffe6774bde638c8e0a05
lallonen <mdl_pedro@hotmail.com>
2015-07-18 18:31:02 +0200

    Despliegue en Heroku

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,17 @@
+
+//GET /QUIZES/QUESTION
+
+exports.question = function(req,res){
+	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+};
+
+
+//GET /QUIZES/ANSWER
+
+exports.answer = function(req, res){
+	if(req.query.respuesta === 'Roma'){
+		res.render('quizes/answer', {respuesta: 'Correcto'});
+	}else{
+		res.render('quizes/answer', {respuesta:'Incorrecto'});
+	}
+};
