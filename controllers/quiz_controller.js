d9daccc42781b1423523e11732177914aef2849f
lallonen <mdl_pedro@hotmail.com>
2015-07-26 13:20:47 +0200

    Añadir listas a proyecto

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,15 +2,36 @@
 var models = require('../models/models.js');
 //GET /QUIZES/QUESTION
 
-exports.question = function(req,res){
+/*exports.question = function(req,res){
 	models.Quiz.findAll().then(function(quiz){
 		res.render('quizes/question', {pregunta: quiz[0].pregunta});
 	})
+};*/
+
+exports.index = function(req,res){
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index.ejs',{quizes : quizes});
+	})
 };
 
+//GET /quizes/:id
+exports.show = function(req,res){
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
+		res.render('quizes/show',{quiz :quiz});
+	})
+};
 
+exports.answer = function(req, res){
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
+		if(req.query.respuesta === quiz.respuesta){
+			res.render('quizes/answer', {quiz: quiz, respuesta:'Correcto'});
+		}else{
+			res.render('quizes/answer', {quiz: quiz, respuesta:'Incorrecto'});
+		}
+	})
+};
+/*
 //GET /QUIZES/ANSWER
-
 exports.answer = function(req, res){
 	models.Quiz.findAll().then(function(quiz){
 		if(req.query.respuesta === quiz[0].respuesta){
@@ -20,7 +41,7 @@ exports.answer = function(req, res){
 		}
 	})
 	
-};
+};*/
 
 //GET /AUTHOR
 exports.author = function(req,res){
